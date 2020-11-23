71e9a1b557be3fd85f18bfb247d19629b7978d7a
Thea <thea.mushambadze@gmail.com>
2020-11-23 22:23:40 +0400

    pics

--- face_detection/script.py
+++ face_detection/script.py
@@ -2,7 +2,7 @@ import cv2
 
 face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
-img=cv2.imread("news.jpg")
+img=cv2.imread("photo.jpg")
 
 gray_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
